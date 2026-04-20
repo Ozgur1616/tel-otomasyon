fcbe29079a2ad3d43626d9d20304125cf1a39f8b
Ozgur1616 <oefe02081@gmail.com>
2026-04-20 17:10:42 +0300

    Update import tkinter as tk.py

--- import tkinter as tk.py
+++ import tkinter as tk.py
@@ -8,9 +8,9 @@ from email.mime.multipart import MIMEMultipart
 # ------------------ MAİL AYARLARI (BURAYI DOLDUR) ------------------
 # Gmail hesabından aldığın 16 haneli uygulama şifresini 'SIFRE' kısmına yaz.
 # Gonderen ve Alici mail adreslerini kendi mailinle değiştir.
-GONDEREN_MAIL = "seninmailin@gmail.com"
-ALICI_MAIL = "siparisin_gidecegi_mail@gmail.com"
-SIFRE = "xxxx xxxx xxxx xxxx" # Buraya o 16 haneli uygulama şifresini yapıştır!
+GONDEREN_MAIL = "oefe02090n@gmail.com"
+ALICI_MAIL = "oefe02081@gmail.com"
+SIFRE = "fmlftvqhyqyrkkhw" # Buraya o 16 haneli uygulama şifresini yapıştır!
 
 def mail_gonder(siparis_detay):
     try:
